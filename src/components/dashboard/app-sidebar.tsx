f9cf9d39ace389a286b2d8af0acc5aac076dc00c
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-08-07 11:11:08 +0100

    chore: set dashboard pages.

--- src/components/dashboard/app-sidebar.tsx
+++ src/components/dashboard/app-sidebar.tsx
@@ -1,181 +1,40 @@
 "use client"
 
+import { NavMain } from '@/components/dashboard/nav-main'
+import { NavUser } from '@/components/dashboard/nav-user'
 import {
-  IconCamera,
-  IconChartBar,
-  IconDashboard,
-  IconDatabase,
-  IconFileAi,
-  IconFileDescription,
-  IconFileWord,
-  IconFolder,
-  IconHelp,
-  IconInnerShadowTop,
-  IconListDetails,
-  IconReport,
-  IconSearch,
-  IconSettings,
-  IconUsers,
-} from "@tabler/icons-react"
-import * as React from "react"
-
-import { NavDocuments } from "@/components/dashboard/nav-documents"
-import { NavMain } from "@/components/dashboard/nav-main"
-import { NavSecondary } from "@/components/dashboard/nav-secondary"
-import { NavUser } from "@/components/dashboard/nav-user"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { ROUTES } from '@/lib/constants'
+import { IconInnerShadowTop } from '@tabler/icons-react'
+import * as React from 'react'
 
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "#",
-      icon: IconDashboard,
-    },
-    {
-      title: "Lifecycle",
-      url: "#",
-      icon: IconListDetails,
-    },
-    {
-      title: "Analytics",
-      url: "#",
-      icon: IconChartBar,
-    },
-    {
-      title: "Projects",
-      url: "#",
-      icon: IconFolder,
-    },
-    {
-      title: "Team",
-      url: "#",
-      icon: IconUsers,
-    },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: IconSettings,
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: IconHelp,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: IconSearch,
-    },
-  ],
-  documents: [
-    {
-      name: "Data Library",
-      url: "#",
-      icon: IconDatabase,
-    },
-    {
-      name: "Reports",
-      url: "#",
-      icon: IconReport,
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: IconFileWord,
-    },
-  ],
+	navMain: ROUTES,
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <a href="#">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavDocuments items={data.documents} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
-    </Sidebar>
-  )
+	return (
+		<Sidebar collapsible='offcanvas' {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem className='flex items-center gap-2 text-primary'>
+						<IconInnerShadowTop className='!size-10' />
+						<span className='text-xl font-bold'>App logo</span>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser />
+			</SidebarFooter>
+		</Sidebar>
+	)
 }
--- src/components/dashboard/site-header.tsx
+++ src/components/dashboard/site-header.tsx
@@ -1,14 +1,23 @@
+'use client'
+
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { ROUTES } from '@/lib/constants'
+import { usePathname } from 'next/navigation'
 import ThemeToggle from '../base/theme-toggle'
 
 export function SiteHeader() {
+	const pathName = usePathname()
+
+	const path = pathName.split('/')[1]
+	const title = ROUTES.find((route) => route.url.split('/')[1] === path)?.title || 'Dashboard'
+
 	return (
 		<header className='flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)'>
-			<div className='flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6'>
+			<div className='flex w-full items-center gap-1 lg:gap-2 px-3'>
 				<SidebarTrigger className='-ml-1' />
 				<Separator orientation='vertical' className='mx-2 data-[orientation=vertical]:h-4' />
-				<h1 className='text-base font-medium'>Documents</h1>
+				<h1 className='text-lg font-bold'>{title}</h1>
 				<div className='ml-auto flex items-center gap-2'>
 					<ThemeToggle />
 				</div>
