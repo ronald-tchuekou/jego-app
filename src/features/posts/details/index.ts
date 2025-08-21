d5504e1ad7b4f3a99cae1abe19236946ed611145
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-08-21 19:53:13 +0100

    chore: new refacto.

--- src/features/posts/details/index.ts
+++
@@ -1,4 +0,0 @@
-export { PostDetails } from './post-details'
-export { PostDetailsAdmin } from './post-details-admin'
-export { PostDetailsUser } from './post-details-user'
-
--- src/features/profile/index.ts
+++
@@ -1,7 +0,0 @@
-// Profile components exports
-export * from './actions'
-export * from './delete-account'
-export * from './update-email'
-export * from './update-image-profile'
-export * from './update-password'
-export * from './update-user-info'
