632d63d81acc83a93f54603886741910ec06333b
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-08-25 12:10:22 +0100

    chore: update app font.

--- src/components/dashboard/site-header.tsx
+++ src/components/dashboard/site-header.tsx
@@ -3,21 +3,37 @@
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { ROUTES } from '@/lib/constants'
+import { Building } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
 import ThemeToggle from '../base/theme-toggle'
+import { useAuth } from '../providers/auth'
 
 export function SiteHeader() {
    const pathName = usePathname()
-
+   const { auth } = useAuth()
+   console.log(auth)
    const path = pathName.split('/')[1]
    const title = ROUTES.find((route) => route.url.split('/')[1] === path)?.title || 'Dashboard'
+
+   const CompanyName = useMemo(
+      () =>
+         auth?.user?.company?.name ? (
+            <span className='text-primary flex gap-2 items-center'>
+               {' '}
+               <Building className='size-5' />
+               {auth?.user?.company?.name}
+            </span>
+         ) : null,
+      [auth]
+   )
 
    return (
       <header className='flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)'>
          <div className='flex w-full items-center gap-1 lg:gap-2 px-3'>
             <SidebarTrigger className='-ml-1' />
             <Separator orientation='vertical' className='mx-2 data-[orientation=vertical]:h-4' />
-            <h1 className='text-lg font-bold'>{title}</h1>
+            <h1 className='text-lg font-bold'>{CompanyName || title}</h1>
             <div className='ml-auto flex items-center gap-2'>
                <ThemeToggle />
             </div>
