bbf33b91b832c2594d5294de5ac101c1ffa1a223
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-09-03 17:33:58 +0100

    chore: set appointment type

--- src/services/company-appointment-request-service.ts
+++ src/services/company-appointment-request-service.ts
@@ -1,14 +1,23 @@
 import { CompanyModel } from './company-service'
 import { UserModel } from './user-service'
 
+export enum AppointmentStatus {
+   PENDING = 'pending',
+   CONFIRMED = 'confirmed',
+   CANCELLED = 'cancelled',
+   COMPLETED = 'completed',
+}
+
 export type CompanyAppointmentRequestModel = {
    id: string
    companyId: string
    userId: string
-   object: string
+   date: string
+   time: string
+   status: AppointmentStatus
+   subject: string
    content: string
    isRead: boolean
-   date: string
    createdAt: string
    updatedAt: string
    company: CompanyModel
